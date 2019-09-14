0b92d1a34ec91ae93258abe952f7105c8bfccd24
SmaLLAlien <vlad_belousov1990@hotmail.com>
2019-09-14 11:57:15 +0300

    Correct webpack config to work with fonts at mode production

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,88 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const PATHS = {
   src: path.join(__dirname, './src'),
   dist: path.join(__dirname, './dist'),
-  assets: '',
+  assets: ''
 }
-module.exports = {
-  entry: {
-    app: `${PATHS.src}/js/index.js`
-  },
 
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: `${PATHS.src}/js/index.js`,
+  },
   output: {
     filename: `${PATHS.assets}js/[name].js`,
     path: PATHS.dist,
-    publicPath: ''
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          name: `/assets/fonts/[name].[ext]`,
-          outputPath: `${PATHS.assets}`
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader:  "css-loader",
-            options: {sourceMap: true}
-          },
-          {
-            loader:  "postcss-loader",
-            options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
-          },
-          {
-            loader:  "sass-loader",
-            options: {sourceMap: true}
-          }
-        ]
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader',
+      options: {
+        name: 'assets/fonts/[name].[ext]'
       }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: `${PATHS.dist}`,
-    overlay: {
-      warnings: true,
-      errors: true
-    },
-    port: 8081,
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }
+      ]
+    }]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].css`,
     }),
     new CopyWebpackPlugin([
-      { from: `${PATHS.src}/assets`, to: `${PATHS.assets}assets` },
-      // { from: PATHS.src + '/static' },
+      { from: `${PATHS.src}/${PATHS.assets}assets`, to: `${PATHS.assets}assets` },
+
     ]),
-    new HtmlWebpackPlugin({
+
+        new HtmlWebpackPlugin({
       hash: false,
       template: `${PATHS.src}/index.html`,
       filename: './index.html',
     })
-  ],
 
+  ],
 }
